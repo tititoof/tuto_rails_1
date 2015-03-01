fb179c6e203476e1e61f19c23e50f04393770323
Christophe Hartmann <chartmann.35@gmail.com>
2015-03-01 08:37:14 +0100

    Add Users

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     resources :comments
   end
   resources :tags
-
+  resources :users
   root 'articles#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150227185100) do
+ActiveRecord::Schema.define(version: 20150228160907) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -47,5 +47,16 @@ ActiveRecord::Schema.define(version: 20150227185100) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",            null: false
+    t.string   "crypted_password"
+    t.string   "salt"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "username"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
